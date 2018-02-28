becec104d591e9ba3d2539367620624b5c09e579
andino <andy.andino93@gmail.com>
2018-02-27 16:39:25 -0800

    (feat) remove block statement in favor of parens per airbnb style

--- spec/server/server.spec.js
+++ spec/server/server.spec.js
@@ -3,14 +3,14 @@ import request from 'supertest';
 import server from '../../server/server';
 
 describe('App Server', () => {
-  test('should respond to /games/:id', () => {
-    return request(server)
+  test('should respond to /games/:id', () => (
+    request(server)
       .get('/games/1234')
-      .expect(200);
-  });
-  test('should 404 to non-existent route', () => {
-    return request(server)
+      .expect(200)
+  ));
+  test('should 404 to non-existent route', () => (
+    request(server)
       .get('/yolo/1235')
-      .expect(404);
-  });
+      .expect(404)
+  ));
 });
