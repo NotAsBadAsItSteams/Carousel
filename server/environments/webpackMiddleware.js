42b8012359efe78a8109537389e63f98e92de4b4
andino <andy.andino93@gmail.com>
2018-03-03 17:58:34 -0800

    (refactor) update webpack config to dev version

--- server/environments/webpackMiddleware.js
+++ server/environments/webpackMiddleware.js
@@ -3,7 +3,7 @@ const mountWebpackMiddleware = (server) => {
   // disable for global requires since airbnb doesn't like them being in a block
   if (process.env.NODE_ENV === 'development') {
     /* eslint-disable */
-    const config = require('../../webpack.config.js');
+    const config = require('../../webpack.dev.config.js');
     const compiler = require('webpack')(config);
     server.use(require('webpack-dev-middleware')(compiler, {
       noInfo: true,
